6617a2338ec1253118fd0438a5cfce5d1f5bfb71
romina <romi.winter@wilcohq.com>
2022-01-24 18:25:12 +0200

    feat - change to populateAllTables, to create users, items and comments altogether

--- backend/seeds.js
+++ backend/seeds.js
@@ -1,40 +1,40 @@
 require("dotenv").config();
 var mongoose = require('mongoose');
+
 require('./models/User');
 require('./models/Item');
+require('./models/Comment');
+
 var Item = mongoose.model('Item');
 var User = mongoose.model('User');
+var Comment = mongoose.model('Comment');
 
-async function populateItemsTable() {
-    const itemssCount = 105;
-    console.log('Populating Items');
+async function populateAllTables(){
+    console.log('Start populating DB');
+    const count = 105;
 
-    const user = new User({ username: 'test', email: 'test@wilcohq.com' });
-    await user.save();
-
-    for (var i=1; i<itemssCount; i++) {
-        var newItem = new Item({ title: `Item ${i}`, description: `This is item ${i} description` });
-        newItem.seller = user;
-        await newItem.save()
-    }
-}
-
-async function populateUsersTable() {
-    const usersCount = 105;
-    console.log('Populating users');
-
-    for (var i=1; i<usersCount; i++){
+    for (var i=1; i<count; i++){
+        // 1. Create user
         const user = new User({ username: `TestUser${i}`, email: `TestUser${i}@wilcohq.com` });
         await user.save();
+
+        // 2. Create item
+        var item = new Item({ title: `Item ${i}`, description: `This is item ${i} description` });
+        item.seller = user;
+        await item.save();
+
+        // 3. Create comment
+        var comment = new Comment({ body: `This is comment ${i}`, seller: user, item });
+        await comment.save();
+
+        // 3.1 connect to comment to item
+        item.comments = item.comments.concat([comment]);
+        await item.save();
     }
 }
 
 mongoose.connect(process.env.MONGODB_URI);
-populateItemsTable().then(()=> {
-    console.log('Done populating Items');
-});
-
-populateUsersTable().then(()=> {
-    console.log('Done populating users');
+populateAllTables().then(() => {
+    console.log('Done populating DB');
     process.exit();
-});
+})
